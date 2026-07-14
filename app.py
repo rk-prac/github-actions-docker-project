f9bf8e8001287e7f5dcdb8324eb2d986e1a0ed12
rahul <rk@gmail.com>
2026-07-14 16:36:35 +0100

    Add Kubernetes ConfigMap support

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Version 2 - Kubernetes Rolling Update Successful!"
+    return os.getenv("APP_MESSAGE", "Default message")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000) 
